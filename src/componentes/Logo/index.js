da91cfd4ea834ab5354a468fe109b5715da65786
jeovana santos <jeovanasantosdacosta25@gmail.com>
2024-08-23 10:50:50 -0400

    primeiro commit

---
+++ src/componentes/Logo/index.js
@@ -0,0 +1,14 @@
+import logo from '../../img/logo.svg'
+import './style.css'
+ 
+function Logo() {
+  return (
+    <div className="logo">
+      <img className='logo-img' src={logo} alt="logo"></img>
+      <p>
+        <strong>Alura</strong>Books
+      </p>
+    </div>
+  );
+}
+export default Logo;
---
+++ src/componentes/OpcoesHeader/index.js
@@ -0,0 +1,15 @@
+
+const textoOpcoes = ['Categorias','Minha Estante','Favorito']
+
+function OpcoesHeader() {
+  return (
+    <ul className="opcoes">
+      {textoOpcoes.map((texto) => (
+        <li className="opcao">
+          <p>{texto}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+export default OpcoesHeader;
