6a2a666d0ea03f8f268d4f5572900b8a4c93cd6a
Jeovana Santos da Costa <jeovanasantosdacosta25@gmail.com>
2024-08-27 16:08:20 -0400

    criação de estados

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,5 +1,6 @@
 import Input from "../Input";
 import styled from "styled-components";
+import { useState } from "react";
 
 const PesquisaContainer = styled.section`
   background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
@@ -23,11 +24,13 @@ const Subtitulo = styled.h3`
 `;
 
 function Pesquisa() {
+    const [textoDigitado, setTextoDigitado] = useState('')
   return (
     <PesquisaContainer>
       <Titulo>Já sabe por onde começar? </Titulo>
       <Subtitulo>Encontre seu livro em nova estante.</Subtitulo>
-      <Input placeholder="Digite a sua proxima leitura" />
+      <Input placeholder="Digite a sua proxima leitura" onBlur={evento =>setTextoDigitado(evento.target.value)}/>
+      <p>{textoDigitado}</p>
     </PesquisaContainer>
   );
 }
