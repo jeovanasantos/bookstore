dba29d0d8eb6fef34740b27bbf5fbc011493726e
Jeovana Santos da Costa <jeovanasantosdacosta25@gmail.com>
2024-08-27 15:50:35 -0400

    criacao do componente de search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Header from './componentes/header';
 import styled from "styled-components";
+import Pesquisa from './componentes/Pesquisa'
 
 const AppContainer = styled.div`
     width: 100vw;
     height: 100vh;
-    background: #363636;
+     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
   
 `;
 
@@ -12,6 +13,7 @@ function App() {
   return (
     <AppContainer>
       <Header></Header>
+      <Pesquisa/>
     </AppContainer>
   );
 }
