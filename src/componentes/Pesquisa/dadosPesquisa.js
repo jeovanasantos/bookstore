43674414a7a8e69edefdf3010cd3ecf450750da6
jeovana santos <jeovanasantosdacosta25@gmail.com>
2024-08-28 08:52:18 -0400

    criação de filtro para livros

---
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -0,0 +1,9 @@
+import livro from '../../img/livro.png';
+
+
+export const livros = [
+    {nome: 'Liderança em Design', id: 1, src:livro},
+    {nome: 'JavaScript', id: 2, src:livro},
+    {nome: 'Liderança e Gestão', id: 3, src:livro},
+    {nome: 'Liderança em Java', id: 2, src:livro}
+]
