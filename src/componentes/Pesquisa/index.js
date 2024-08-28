8bc36b20a9e307c5fde0e7e2818d905f9e897ec7
jeovana santos <jeovanasantosdacosta25@gmail.com>
2024-08-28 09:39:54 -0400

    Impressão de imagens no navegador

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,7 +1,7 @@
 import Input from "../Input";
 import styled from "styled-components";
 import { useState } from "react";
-import{ livros} from './dadosPesquisa'
+import { livros } from "./dadosPesquisa";
 
 const PesquisaContainer = styled.section`
   background-image: linear-gradient(90deg, #002f52 35%, #326589 165%);
@@ -23,23 +23,46 @@ const Subtitulo = styled.h3`
   font-weight: 500;
   margin-bottom: 40px;
 `;
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+    p {
+        width: 200px;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
+`
 
 function Pesquisa() {
-    const [livrosPesquisados, setLivrosPesquisados] = useState([])
+  const [livrosPesquisados, setLivrosPesquisados] = useState([])
 
-    console.log(livrosPesquisados);
   return (
     <PesquisaContainer>
       <Titulo>Já sabe por onde começar? </Titulo>
       <Subtitulo>Encontre seu livro em nova estante.</Subtitulo>
-      <Input placeholder="Digite a sua proxima leitura" 
-      onBlur={evento => {
-        const textoDigitado = evento.target.value
-        const resultadoPesquisa = livros.filter(livro => livro.nome.includes(textoDigitado))
-        setLivrosPesquisados(resultadoPesquisa);
-      }
-      }/>
-      
+      <Input
+        placeholder="Digite a sua proxima leitura"
+        onBlur={(evento) => {
+          const textoDigitado = evento.target.value;
+          const resultadoPesquisa = livros.filter(livro =>
+            livro.nome.includes(textoDigitado)
+          )
+          setLivrosPesquisados(resultadoPesquisa);
+        }}
+      />
+      {livrosPesquisados.map(livro => (
+        <Resultado>
+          <p>{livro.nome}</p>
+          <img src={livro.src} />
+        </Resultado>
+      ))}
     </PesquisaContainer>
   );
 }
